4c5860ec529876b52e86a38c447d6fb686a27ecd
fikrihashfi <fikrihashfi@gmail.com>
2019-11-23 11:34:00 +0700

    new

--- application/views/index.php
+++ application/views/index.php
@@ -65,7 +65,7 @@
 
                                    <h1 class="text-white" data-aos="fade-up">We are P<span style="color:red">!</span>NGFEST</h1>
 
-                                   <a href="contact.html" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Let us discuss together!</a>
+                                   <a href="#footer" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Let us discuss together!</a>
 
                                    <strong class="d-block py-3 pl-5 text-white" data-aos="fade-up" data-aos-delay="200"><i class="fa fa-phone mr-2"></i> +99 080 070 4224</strong>
                               </div>
@@ -93,8 +93,8 @@
 
                               <h2 class="mb-4" data-aos="fade-up">The best <strong>Event</strong> in Indonesia</h2>
 
-                              <p class="mb-0" data-aos="fade-up">Total 5 HTML pages are included in this template from TemplateMo website. Please check 2 <a href="blog.html">blog</a> pages, <a href="project-detail.html">project</a> page, and <a href="contact.html">contact</a> page. 
-                              <br><br>You are <strong>allowed</strong> to use this template for commercial or non-commercial purpose. You are NOT allowed to redistribute the template ZIP file on template collection websites.</p>
+                              <p class="mb-0" data-aos="fade-up">P!ng merupakan kependekan dari pekan informasi dan teknologi yang diadakan 1 kali setiap tahunnya. <a href="pingfest.id">P!NG</a> pages. 
+                              <br><br>Anda<strong>diperbolehkan</stong> untuk menggunakan template ini secara komersial.</p>
                          </div>
 
                          <div class="about-image" data-aos="fade-up" data-aos-delay="200">
